492c7b5a0d47ff574ceaf0433f882c775aa334e9
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2022-11-03 21:17:05 -0600

    Delete members

--- routes/api/members.js
+++ routes/api/members.js
@@ -13,7 +13,7 @@ router.get('/', (req,res) =>{
     const found =members.some(member=> member.id === parseInt(req.params.id)) 
  
  if (found){
-     res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
  } else{
     res.status(400).json({msg: `No member with the id of ${req.params.id}`}) 
  }
@@ -57,7 +57,22 @@ router.put('/:id', (req,res) =>{
   
  })
 
- //delete
+ //delete Member
+ router.delete('/:id', (req,res) =>{
+    const found =members.some(member=> member.id === parseInt(req.params.id)) 
+ 
+ if (found){
+  
+
+     res.json({ 
+        msg: 'Member deleted',
+        members: members.filter(member => member.id !== parseInt(req.params.id))});
+ } else{
+    res.status(400).json({msg: `No member with the id of ${req.params.id}`}) 
+ }
+  
+ })
+
 
 
 
