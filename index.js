cdd6a2581c8c88e01ba1f0bc1f05f506ca6a0cd8
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2022-11-03 22:15:50 -0600

    using form

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express');
-const path = require('path')
-const logger = require('./middleware/logger')
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members')
 
 const app = express();
 
 // //Init middleware
 // app.use(logger);
 
+// Handlebar Middleware
+app.engine('handlebars', exphbs.engine({ defaultLayout: "main" }))
+app.set('view engine', 'handlebars');
+
 //Body Parser to translate data from the post request
 app.use(express.json());
 app.use(express.urlencoded({ extended: false})); 
 
-
+// Homepafe route
+app.get('/', (req, res)=>{
+    res.render('index',{
+        title: 'Member App',
+        members
+    })
+});
 
 //Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
